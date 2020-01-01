4b5a9c8941181506dc1db28453b3ccab8eb9e362
Andrew Geiger <andrew.geiger@corsosystems.com>
2020-01-01 15:47:28 -0500

    prepping for upload to PyPI

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="plastic",
+    version="0.1.0",
+    author="Andrew Geiger",
+    author_email="andrew.geiger@corsosystems.com",
+    description="An ORM interface that molds itself to your data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CorsoSource/plastic",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
